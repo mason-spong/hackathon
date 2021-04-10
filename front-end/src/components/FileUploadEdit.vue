33a97dc899f3b4b0e339da2b5ca889498a30b84e
Mason Spong <mason@Masons-Air.app.byu.edu>
2021-04-10 17:06:08 -0600

    add FileUploadEdit styles

--- front-end/src/components/FileUploadEdit.vue
+++ front-end/src/components/FileUploadEdit.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="file-upload-edit">
-    <div class="file-upload">
-      <h2>Describe what type of file will be uploaded</h2>
-      <input v-model="moduleData.questionDescription" />
-      
+  <div class="file-upload-edit module-wrap">
+    <div class="module-content">
+      <div class="file-upload">
+        <h2>Describe what type of file will be uploaded</h2>
+        <textarea placeholder="Please upload..." class="large-textarea" v-model="moduleData.questionDescription" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "FileUploadEdit",
   props: {
-    moduleData: Object
+    moduleData: Object,
   },
-  
 };
 </script>
