79e2eab5b6773145f78421d40f6c7b79acee1873
Sam Bennion <sambennion3@gmail.com>
2021-04-10 17:44:13 -0600

    merging let's hope

--- front-end/src/views/ApplicationEdit.vue
+++ front-end/src/views/ApplicationEdit.vue
@@ -3,15 +3,9 @@
     <div id="nav">
       <router-link to="/launchpad">Return to Launchpad</router-link>
     </div>
-<<<<<<< HEAD
-    <h2 v-if="company" class="companyName">
-      Logged in as: {{ company.companyName }}
-      <button @click="logout" class="pure-button pure-button-primary">
-=======
     <div class="company-info" v-if="company">
       <h2 class="company-name">{{ company.companyName }}</h2>
       <button class="logout" @click="logout" >
->>>>>>> 03c7b3fd01022237539f01ed0cdb01c145fbd64d
         Logout
       </button>
     </div>
@@ -24,74 +18,14 @@
       v-bind:is="comp.component"
       :moduleData="comp"
     ></component>
-<<<<<<< HEAD
-    <button @click="addFreeResponseModuleEdit" class="pure-button">Add free response</button>
-    <button @click="addMultipleChoiceModuleEdit" class="pure-button1">Add Multiple Choice</button>
-    <button @click="addCodingQuestionEdit" class="pure-button1">Add Coding Question</button>
-    <button @click="addFileUploadEdit" class="pure-button1">Add File Upload Question</button>
-    <button @click="submit" class="pure-button1">{{submitText}}</button>
-=======
     <button @click="addFreeResponseModuleEdit">Add free response</button>
     <button @click="addMultipleChoiceModuleEdit">Add Multiple Choice</button>
     <button @click="addCodingQuestionEdit">Add Coding Question</button>
     <button @click="addFileUploadEdit">Add File Upload Question</button>
     <button @click="submit">{{ submitText }}</button>
->>>>>>> 03c7b3fd01022237539f01ed0cdb01c145fbd64d
   </div>
 </template>
-<style scoped>
-* {
-    box-sizing: border-box;
-  }
 
-#nav {
-  font-size: 2rem;
-}
-
-h2 {
-  font-size: 4rem;
-}
-
-.companyName {
-  font-size: 5rem;
-  color: #86C232;
-}
-
-.pure-button {
-  justify-self: center;
-  background-color: #86C232;
-  position: absolute;
-  top: 15px;
-  padding: .5rem;
-  border-radius: 8px;
-  font-size: 2.5rem;
-  border: 1px;
-  box-shadow: 0px 0px 2px 0px;
-  transition: .3s;
-}
-
-.pure-button1 {
-  justify-self: center;
-  background-color: #86C232;
-  padding: .5rem;
-  border-radius: 8px;
-  font-size: 2rem;
-  margin: .5rem;
-  box-shadow: 0px 0px 2px 0px;
-  transition: .3s;
-}
-
-.edit:hover, .delete:hover, .create-application:hover, .pure-button:hover {
-    box-shadow: 0px 10px 8px 0px grey;
-  }
-/*
-textarea,
-input {
-  border-radius: 8px;
-  padding: 6px;
-}*/
-
-</style>
 <script>
 import axios from "axios";
 // import FreeResponseQuestion from "@/components/FreeResponseQuestion.vue";
